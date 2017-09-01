6e567a01958f7af64d9e497c745d19b3721d8073
胡鹏 <750577590@qq.com>
2017-09-01 23:31:16 +0800

    搜索分页修改

--- src/main/resources/static/dist/js/page/userManage.js
+++ src/main/resources/static/dist/js/page/userManage.js
@@ -1,5 +1,5 @@
 var api = api+'userManage/';
-
+var modIdArr = [];
 $(function () {
     initBunding();
     initTable()
@@ -25,9 +25,10 @@ function initTable() {
         data = data.datas==null?'':data.datas;
         if(data!=""){
             $.each(data,function (i,value) {
+                modIdArr.push(value.MOD_ID);
                 var modType = value.MOD_TYPE==1?'1-普通':'2-特许';
                 var visitRole = value.VISIT_ROLE==1?'可用':'不可用';
-                var registRole = value.modType==1?'可用':'不可用';
+                var registRole = value.REGIST_ROLE==1?'可用':'不可用';
                 var serverCost =value.SERVER_COST==null?'':value.SERVER_COST+'元';
                 var serverNum = value.SERVER_NUM==null?'':value.SERVER_NUM+'条';
                 var startDate = value.START_DATE==null?'':value.START_DATE;
@@ -71,10 +72,55 @@ function initTable() {
         layer.closeAll();
         $('.editList').click(function () {
             $('#myModal').modal('show');
-
+            $('.modal-title').text('修改权限');
+            var BELONG_WEB = $(this).parent().parent().find('div').eq(0).text();
+            var MOD_ID = $(this).parent().parent().find('div').eq(1).text();
+            var MOD_NAME = $(this).parent().parent().find('div').eq(2).text();
+            var modType = $(this).parent().parent().find('div').eq(3).text();
+            var visitRole = $(this).parent().parent().find('div').eq(4).text();
+            var registRole = $(this).parent().parent().find('div').eq(5).text();
+            var serverCost = $(this).parent().parent().find('div').eq(6).text();
+            var serverNum = $(this).parent().parent().find('div').eq(7).text();
+            $('#BELONG_WEB').val(BELONG_WEB);
+            $('#MOD_ID').val(MOD_ID);
+            $('#MOD_NAME').val(MOD_NAME);
+            if(modType=='1-普通') {
+                $('input[name="modType"]:eq(0)').attr("checked",'checked');
+            }else{
+                $('input[name="modType"]:eq(1)').attr("checked",'checked');
+            }
+            if(visitRole=='可用') {
+                $('input[name="visitRole"]:eq(0)').attr("checked",'checked');
+            }else{
+                $('input[name="visitRole"]:eq(1)').attr("checked",'checked');
+            }
+            if(registRole=='可用') {
+                $('input[name="registRole"]:eq(0)').attr("checked",'checked');
+            }else{
+                $('input[name="registRole"]:eq(1)').attr("checked",'checked');
+            }
+            $('#serverCost').val(serverCost.replace("元",""));
+            $('#serverNum').val(serverNum.replace("条",""));
         });
         $('.codebtn').click(function () {
+            $('#BELONG_WEB').val("");
+            $('#MOD_ID').val("");
+            $('#MOD_NAME').val("");
+            $('input[name="modType"]:radio:checked').val();
+            $('input[name="visitRole"]:radio:checked').val();
+            $('input[name="registRole"]:radio:checked').val();
+            $('#serverCost').val("");
+            $('#serverNum').val("");
             $('#myModal').modal('show');
+            $('.modal-title').text('新增权限');
+            $('#MOD_ID').unbind('blur');
+            $('#MOD_ID').blur(function () {
+                var MOD_ID = $('#MOD_ID').val();
+                if(modIdArr.indexOf(parseInt(MOD_ID))>-1){
+                    $('#MOD_ID').val("");
+                    layer.msg("功能ID已存在,请从新输入!");
+                }
+            });
         });
         $('.delList').click(function () {
             var modId = $(this).parent().parent().find('.modId').text();
@@ -83,6 +129,51 @@ function initTable() {
                 layer.msg(data.info);
             });
             $(this).parent().parent().remove();
+        });
+        $('#sureBtn').click(function () {
+            var BELONG_WEB = $('#BELONG_WEB').val();
+            var MOD_ID = $('#MOD_ID').val();
+            var MOD_NAME = $('#MOD_NAME').val();
+            var modType = $('input[name="modType"]:radio:checked').val();
+            var visitRole = $('input[name="visitRole"]:radio:checked').val();
+            var registRole = $('input[name="registRole"]:radio:checked').val();
+            var serverCost = $('#serverCost').val();
+            var serverNum = $('#serverNum').val();
+            if(BELONG_WEB!=""&&MOD_ID!=""&&MOD_NAME!=""&&modType!=""&&visitRole!=""&&registRole!="") {
+                if($('.modal-title').text().indexOf('新增')>-1) {
+                    var url = api + 'addMod?BELONG_WEB=' + encodeURI(BELONG_WEB) +
+                        '&modId=' + encodeURI(MOD_ID) +
+                        '&MOD_NAME=' + encodeURI(MOD_NAME) +
+                        '&modType=' + encodeURI(modType) +
+                        '&visitRole=' + encodeURI(visitRole) +
+                        '&registRole=' + encodeURI(registRole) +
+                        '&serverCost=' + encodeURI(serverCost) +
+                        '&serverNum=' + encodeURI(serverNum);
+                    $.getJSON(url, function (data) {
+                        $('#myModal').modal('hide');
+                        layer.msg(data.info);
+                    }).complete(function () {
+                        initTable();
+                    });
+                }else{
+                    var url = api + 'editMod?BELONG_WEB=' + encodeURI(BELONG_WEB) +
+                        '&modId=' + encodeURI(MOD_ID) +
+                        '&MOD_NAME=' + encodeURI(MOD_NAME) +
+                        '&modType=' + encodeURI(modType) +
+                        '&visitRole=' + encodeURI(visitRole) +
+                        '&registRole=' + encodeURI(registRole) +
+                        '&serverCost=' + encodeURI(serverCost) +
+                        '&serverNum=' + encodeURI(serverNum);
+                    $.getJSON(url, function (data) {
+                        $('#myModal').modal('hide');
+                        layer.msg(data.info);
+                    }).complete(function () {
+                        initTable();
+                    });
+                }
+            }else{
+                layer.msg("页面模块名或功能ID或功能名称或权限类别或游客是否可用或注册是否可以不能为空!")
+            }
         });
     });
 }
